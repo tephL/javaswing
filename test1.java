15d3153b9e4f2fb6a059b23ae735306d12124a50
tephL <rhezstephen06@gmail.com>
2026-01-16 00:08:22 +0800

    added comments that explains the code

--- test1.java
+++ test1.java
@@ -7,16 +7,23 @@ public class Test1 {
     
     public static void main(String[] args) {
         JFrame f = new JFrame("Event");
-        JButton b = new JButton("Button 1");
-        JButton b2 = new JButton("Button 2");
+        JButton b = new JButton("Button 1");  //make a button
+        JButton b2 = new JButton("Button 2");  //make 2nd button
         
         b.addActionListener(e->System.out.println("clicked"));
+        //if button b is clicked, this will be printed out
         
-        f.add(b);
+        //add the buttons within the frame
+        f.add(b); 
         f.add(b2);
         
+        //declare frame (window) size
         f.setSize(300, 350);
+
+        // exiter
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //shows the frame as a window
         f.setVisible(true);
         
     }
